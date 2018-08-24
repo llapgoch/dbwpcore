ab3f909062851193bedd8b4772b0b562edd40917
Dave Baker <dave_preece@mac.com>
2018-08-24 11:32:21 +0100

    Fix magic quote nonsense

--- vendor/DaveBaker/Core/App/Request.php
+++ vendor/DaveBaker/Core/App/Request.php
@@ -2,6 +2,11 @@
 
 namespace DaveBaker\Core\App;
 
+/**
+ * Class Request
+ * @package DaveBaker\Core\App
+ *
+ */
 class Request
 {
     const GET = "get";
@@ -14,20 +19,35 @@ class Request
         self::CUSTOM => []
     ];
 
+
     public function __construct()
     {
-        // Parse parameters
+        $this->compileParams();
+    }
+
+
+    /**
+     * @return $this
+     *
+     * Compiles a local registry of requests.
+     * Undo Wordpress' Magic Quote nonsense.
+     */
+    protected function compileParams()
+    {
+
         if(is_array($_GET)){
             foreach($_GET as $k => $param){
-                $this->params[self::GET][$k] = $param;
+                $this->params[self::GET][$k] = stripslashes_deep($param);
             }
         }
 
         if(is_array($_POST)){
             foreach($_POST as $k => $param){
-                $this->params[self::POST][$k] = $param;
+                $this->params[self::POST][$k] = stripslashes_deep($param);
             }
         }
+
+        return $this;
     }
 
     /**
