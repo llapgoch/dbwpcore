0f58cf4e1411889ab8159e8d52d5a072f0d56f11
Dave Baker <dave_preece@mac.com>
2018-08-27 15:26:16 +0100

    Messages block

---
+++ vendor/DaveBaker/Core/Block/Html/Messages.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace DaveBaker\Core\Block\Html;
+/**
+ * Class Messages
+ * @package DaveBaker\Core\Block\Html
+ */
+class Messages extends Base
+{
+    protected function init()
+    {
+        $this->setTemplate('html/messages.phtml');
+    }
+
+    /**
+     * @return array
+     * @throws \DaveBaker\Core\Object\Exception
+     */
+    public function getMessages()
+    {
+        return $this->getApp()->getGeneralSession()->getMessages();
+    }
+}
