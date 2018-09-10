f69851c64f39f159c96784f0403a08d11f3514e9
Dave Baker <dave_preece@mac.com>
2018-09-10 13:56:29 +0100

    Block replacer JS

--- assets/vendor/dbwpcore/js/block-replacer.js
+++ assets/vendor/dbwpcore/js/block-replacer.js
@@ -1,5 +1,16 @@
 ;(function($){
-	$(document).on('ajaxComplete', function(data){
-		console.log(data);
+	var BLOCK_REPLACERS_DATA_KEY = '__block__replacers__';
+
+	$(document).on('ajaxComplete', function(event, request){
+		var response = request.responseJSON;
+		var replacers = response[BLOCK_REPLACERS_DATA_KEY];
+
+		if(!replacers){
+			return;
+		}
+
+		for(var i in replacers){
+			$('[data-' + BLOCK_REPLACERS_DATA_KEY + '="' + i + '"]').replaceWith(replacers[i]);
+		}
 	});
 }(jQuery));
