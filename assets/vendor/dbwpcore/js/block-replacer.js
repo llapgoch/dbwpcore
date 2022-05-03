db01b72566675679e7c0f4c24a9222ce4bd2375a
Dave Baker <dave@awaredigital.co.uk>
2022-05-03 17:19:38 +0100

    Added events to block replacer

--- assets/vendor/dbwpcore/js/block-replacer.js
+++ assets/vendor/dbwpcore/js/block-replacer.js
@@ -1,7 +1,8 @@
 ;(function($){
     var BLOCK_REPLACERS_DATA_KEY = '__block__replacers__';
+    var $document = $(document)
 
-    $(document).on('ajaxSuccess', function(event, request){
+    $document.on('ajaxSuccess', function(event, request){
         var response = request.responseJSON;
 
         if(!response || !response[BLOCK_REPLACERS_DATA_KEY]){
@@ -16,14 +17,16 @@
             }
 
             var newBlock = $(replacers[i]);
+            var eventData = {newBlock, code: i};
 
             // Trigger an event on the old block, pass in the new block so that any setup can be init'd
             $('[data-' + BLOCK_REPLACERS_DATA_KEY + '="' + i + '"]')
-                .trigger('block-replacer-before', {newBlock:newBlock})
+                .trigger('block-replacer-before', eventData)
                 .replaceWith(newBlock);
             
+            $document.trigger('block.replacer.item.after.' + i, eventData);
         }
 
-        $(document).trigger('blockReplacerComplete');
+        $document.trigger('blockReplacerComplete');
     });
 }(jQuery));
